d42626256269957aab20e5663a73ec847f3bfb15
Charles <cwlgadd@gmail.com>
2025-01-28 11:48:33 +0000

    Add database code and working demo of building database

---
+++ database/__init__.py
@@ -0,0 +1,4 @@
+from . import build_static_table
+from . import build_diagnosis_table
+from . import build_valued_event_tables
+from . import queries
--- example_data/baseline/README.md
+++
@@ -1,2 +0,0 @@
-# SurvivEHR-ExampleData
-Example placeholder dataset for the SurvivEHR Foundation Model
